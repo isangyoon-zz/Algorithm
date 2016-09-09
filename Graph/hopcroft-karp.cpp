e6a2c8d63c57413a67428d3502712b9a7be1fe1f
Sangyoon Lee <loadbalance@naver.com>
2016-09-09 14:29:33 +0900

    fix hopcroft-karp algorithm

--- Graph/hopcroft-karp.cpp
+++ Graph/hopcroft-karp.cpp
@@ -7,15 +7,20 @@ struct hopcroft_karp
   
   hopcroft_karp(std::size_t n, std::size_t m) : n(n), m(m)
   {
-    adj.resize(n + 1);
-    matching.resize(n + 1, -1);
-    distance.resize(n + 1, -1);
+    adj.resize(n);
+    matching.resize(n, -1);
+    distance.resize(n, -1);
+  }
+  
+  void add_edge(int u, int v)
+  {
+    adj[u].push_back(v);
   }
   
   void bfs()
   {
     std::queue<int> q;
-    for (auto i = 1; i <= n; ++i)
+    for (auto i = 0; i < n; ++i)
     {
       if (matching[i] == -1)
       {
@@ -29,54 +34,42 @@ struct hopcroft_karp
       }
     }
     
-    distance[0] = -1;
-    
     while (!q.empty())
     {
       auto u = q.front();
       q.pop();
       
-      if (u != -1)
+      for (auto v : adj[u])
       {
-        for (auto v : adj[u])
-        {
-          if (distance[matching[v]] < 0)
-          {
-            distance[matching[v]] = distance[u] + 1;
-            
-            q.push(matching[v]);
-          }
-        }
+        if (distance[matching[v]] == -1) return true;
+        if (distance[matching[v]] != -1) continue;
+        
+        distance[matching[v]] = distance[u] + 1;
+        
+        q.push(matching[v]);
       }
     }
     
-    return (distance[0] != -1);
+    return false;
   }
   
   bool dfs(int u)
   {
-    if (u != 0)
+    for (auto v : adj[u])
     {
-      for (auto v : adj[u])
+      if (distance[matching[v]] == distance[u] + 1)
       {
-        if (distance[matching[v]] == distance[u] + 1)
+        if (dfs(matching[v]))
         {
-          if (dfs(matching[v]))
-          {
-            matching[u] = v;
-            matching[v] = u;
-            
-            return true;
-          }
+          matching[u] = v;
+          matching[v] = u;
+          
+          return true;
         }
       }
-      
-      distance[u] = -1;
-      
-      return false;
     }
     
-    return true;
+    return false;
   }
   
   int match()
@@ -84,7 +77,7 @@ struct hopcroft_karp
     int answer = 0;
     while (bfs())
     {
-      for (auto i = 0; i <= n; ++i)
+      for (auto i = 0; i < n; ++i)
       {
         if (dfs(i) && matching[i] == 0) ++answer;
       }
