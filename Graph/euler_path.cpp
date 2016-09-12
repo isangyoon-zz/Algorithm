52ebca8e4beb10bb4853371afdbec92f5b99bb4f
Sangyoon Lee <loadbalance@naver.com>
2016-09-12 10:48:24 +0900

    fix euler path

--- Graph/euler_path.cpp
+++ Graph/euler_path.cpp
@@ -33,7 +33,7 @@ public:
     _paths.push_back(u);
   }
   
-  auto paths() { return _paths.back(); }
+  std::vector<int> paths() { return _paths; }
   
 protected:
   struct _edge
@@ -49,3 +49,28 @@ private:
   std::vector<int> _paths;
 };
 
+/*
+int main(int const argc, char const** argv)
+{
+  std::ios::sync_with_stdio(false);
+  std::cin.tie(0);
+  
+  euler_path euler;
+  
+  euler.add_edge(1, 2);
+  euler.add_edge(2, 3);
+  euler.add_edge(3, 4);
+  euler.add_edge(4, 1);
+  euler.add_edge(1, 3);
+  
+  euler.find_path(1);
+  
+  auto paths = euler.paths();
+  
+  // expected 3 2 1 4 3 1
+  std::copy(std::begin(paths), std::end(paths), std::ostream_iterator<int>(std::cout, " "));
+  std::cout << "\n";
+  
+  return 0;
+}
+*/
