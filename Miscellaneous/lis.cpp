48dc377da3298f6ccd2c3fcfe3f7e7eaf4dde889
Sangyoon Lee <loadbalance@naver.com>
2016-09-24 13:44:24 +0900

    Update lis.cpp

--- Miscellaneous/lis.cpp
+++ Miscellaneous/lis.cpp
@@ -1,19 +1,19 @@
 // find longest increasing subsequence(LIS) size in O(N*logN) time.
-std::size_t lis()
+std::size_t lis() // But it spend more times.
 {
   std::multiset<int> answer;
   for (int i = 0; i < N; ++i)
   {
     answer.insert(A[i]);
     
-    auto it = answer.upper_bound(A[i]);
+    auto it = std::upper_bound(std::begin(answer), std::end(answer), A[i]);
     if (it != std::end(answer)) answer.erase(it);
   }
   
   return answer.size();
 }
 
-std::size_t lis()
+std::size_t lis() // Good! very nice!! i love this solution.
 {
   std::vector<int> answer(1, A[0]);
   
