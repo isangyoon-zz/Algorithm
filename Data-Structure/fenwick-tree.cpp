eda78eda4eed3cfabddd9cc1ca7dd97bcb0bcef2
Sangyoon Lee <loadbalance@naver.com>
2016-09-24 12:44:29 +0900

    Update fenwick-tree.cpp

--- Data-Structure/fenwick-tree.cpp
+++ Data-Structure/fenwick-tree.cpp
@@ -1,5 +1,22 @@
 // Fenwick Tree (BIT) using STL vector
+// using fenwick_tree::query, fenwick_tree::update
+namespace fenwick_tree
+{
+  const int SIZE = 100000;
+  int tree[SIZE + 1];
+  
+  int query(int p)
+  {
+    int answer = 0;
+    for (; p > 0; p -= p & -p) answer += tree[p];
+    
+    return answer;
+  }
+  
+  void update(int p, int value) { for (; p <= SIZE; p += p & -p) tree[p] += value; }
+}
 
+// don't use it!
 template <typename T>
 class fenwick_tree
 {
@@ -8,19 +25,19 @@ private:
   typedef T value_type;
   
 public:
-  fenwick_tree(size_type size = 0) : _size(size), _tree(std::vector<value_type>(size)) {}
+  fenwick_tree(size_type size = 0) : _size(size + 1), _tree(std::vector<value_type>(size + 1)) {}
   fenwick_tree(std::vector<value_type> const& tree) : _size(tree.size()), _tree(tree) {}
   
   void assign(size_type size, value_type default_value = 0)
   {
-    _size = size;
-    _tree.assign(size, default_value);
+    _size = size + 1;
+    _tree.assign(size + 1, default_value);
   }
   
   void resize(size_type size, value_type default_value = 0)
   {
-    _size = size;
-    _tree.resize(size, default_value);
+    _size = size + 1;
+    _tree.resize(size + 1, default_value);
   }
   
   void update(int p, value_type value)
